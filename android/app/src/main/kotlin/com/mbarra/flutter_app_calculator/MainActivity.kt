e81d8cc3006cdbf2686fd50242c3b5b0ee9e5de6
muhammadbarrafirdaus <firdausbarra08@gmail.com>
2021-02-25 16:27:45 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/mbarra/flutter_app_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mbarra.flutter_app_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_app_calculator/calculator.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: Calculator(),
+    );
+  }
+}
